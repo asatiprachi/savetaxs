e7788424ca6e598e89148fcf1359cb4d3067d39f
prachiasati <prachiasati23@gmail.com>
2020-06-05 17:37:16 +0530

    modify login register

---
+++ app/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    //
+    protected $table = "order";
+    protected $fillable = [
+        'email', 'phonenumber','city','state','language',
+    ];
+
+    protected $hidden = [
+         'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
